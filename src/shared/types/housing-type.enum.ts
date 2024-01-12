94473b4bb58704e4e198c20db40e3151fbdda23f
tyshenko61 <34244584+tyshenko61@users.noreply.github.com>
2024-01-12 22:03:33 +0500

    создает описание типов для сущностей

---
+++ src/shared/types/housing-type.enum.ts
@@ -0,0 +1,6 @@
+export enum TypeOfHousing {
+  Apartment = 'apartment',
+  House = 'house',
+  Room = 'room',
+  Hotel = 'hotel',
+}
---
+++ src/shared/types/offer.type.ts
@@ -0,0 +1,22 @@
+import { TypeOfHousing } from './housing-type.enum.js';
+import { User } from './user.type.js';
+
+export type Offer = {
+  title: string;
+  description: string;
+  postDate: Date;
+  city: string;
+  image: string;
+  photos: string[];
+  isPremium: boolean;
+  isFavourites: boolean;
+  rating: number;
+  typeOfHousing: TypeOfHousing;
+  roomsCount: number;
+  guestsCount: number;
+  price: number;
+  comforts: string;
+  user: User;
+  commentsCount: number;
+  coordinates: string;
+}
